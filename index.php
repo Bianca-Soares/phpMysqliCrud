2293a674e51badd7f3e837efb8357e851e7dc497
Bianca-Soares <bianca.soares@sudam.intra>
2021-02-18 16:59:16 -0300

    ajax edit

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
   <meta charset="UTF-8">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <title>Crud PHP com MySQli</title>
-  <script
-  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"
   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
   crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/jquery.mask.min.js"></script>
@@ -13,9 +12,33 @@
   <script type="text/javascript">
       $(document).ready(function(){
           $(document).ready(function(){
-        $("#telefone").mask("(00) 0000-0000");
-        });
+            $("#telefone").mask("(00) 0000-0000");
+          });
+
+          $(".btnRegistrar").click(function(){
+            
+          });
       });
+  </script>
+
+  <script>
+  
+  var dados = new FormData();
+  
+  dados.append('nome', 'daniel');
+  dados.append('idade', 19);
+  dados.append('endereco', 'rua Lobato');
+
+    $.ajax({
+        url: 'index.php',
+        method: 'POST', 
+        data: dados,
+        processData: false,
+        contentType: false,
+        sucess: function(resposta){
+            console.log(resposta);
+        }
+    })
   </script>
 
  </head>
@@ -39,7 +62,7 @@
                 <label for="endereco">Endereço: </label>
                 <input type="text" class="form-control" id="endereco" name="endereco" placeholder="Logradouro, Número da Casa" required>
             </div>
-            <button class="btn btn-primary" type="submit">Registrar</a>
+            <button class="btnRegistrar btn btn-primary" type="submit">Registrar</a>
         </form>
     </div> 
  </body>
